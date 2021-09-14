361fea0b3e735225341444e5806d8d5918c2622e
unknown <arsalan1998m@gmail.com>
2021-09-15 02:17:20 +0500

    learning partials

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+// * mean look any of the file name with sass extension compile it
 function buildStyles() {
-  return src('index.scss').pipe(sass()).pipe(dest('css'));
+  return src('*.scss').pipe(sass()).pipe(dest('css'));
 }
 
 function wathcTasks() {
-  watch(['index.scss'], buildStyles);
+  watch(['*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, wathcTasks);
