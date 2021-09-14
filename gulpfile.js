6bff2d98895a52fd8cd5ad5c5314fda32962d09e
unknown <arsalan1998m@gmail.com>
2021-09-15 01:59:44 +0500

    Learning varibles

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return src('index.scss').pipe(sass()).pipe(dest('css'));
+}
+
+function wathcTasks() {
+  watch(['index.scss'], buildStyles);
+}
+
+exports.default = series(buildStyles, wathcTasks);
